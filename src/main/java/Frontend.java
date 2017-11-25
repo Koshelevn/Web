d2e6019ebbbb110379dec6dbd51b242b6ad375ca
Nikita Koshelev <koshelev.nd@gmail.com>
2017-11-26 01:35:21 +0200

    Initial commit

---
+++ src/main/java/Frontend.java
@@ -0,0 +1,14 @@
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Frontend extends HttpServlet {
+    private String login;
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) {
+
+    }
+    public void doPost(HttpServletRequest request, HttpServletResponse response){
+
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+import Servlets.AllRequestServlet;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        AllRequestServlet frontend = new AllRequestServlet();
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.addServlet(new ServletHolder(frontend), "/*");
+        Server server = new Server(8080);
+        server.setHandler(context);
+        server.start();
+        server.join();
+    }
+}
