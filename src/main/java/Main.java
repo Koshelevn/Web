21dfd4b35cfcbfc1b862016974c3291ce78cfc7b
Nikita Koshelev <koshelev.nd@gmail.com>
2017-11-28 17:49:48 +0200

    Authorization

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,34 @@
-import Servlets.AllRequestServlet;
+import servlets.SessionsServlet;
+import servlets.UsersServlet;
+import accounts.AccountService;
+import accounts.UserProfile;
+import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        AllRequestServlet frontend = new AllRequestServlet();
+        AccountService accountService = new AccountService();
+        
+        accountService.addNewUser(new UserProfile("test"));
+
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        context.addServlet(new ServletHolder(frontend), "/*");
+        context.addServlet(new ServletHolder(new UsersServlet(accountService)), "/api/v1/users");
+        context.addServlet(new ServletHolder(new SessionsServlet(accountService)), "/api/v1/sessions");
+
+        ResourceHandler resource_handler = new ResourceHandler();
+        resource_handler.setResourceBase("public_html");
+
+        HandlerList handlers = new HandlerList();
+        handlers.setHandlers(new Handler[]{resource_handler, context});
+
         Server server = new Server(8080);
-        server.setHandler(context);
+        server.setHandler(handlers);
+
         server.start();
         server.join();
     }
